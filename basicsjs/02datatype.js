ed4f20fbe7c81b395530457fb7a05677bf7dbd10
Ajay Narwariya <100603751+ajaynarwariya@users.noreply.github.com>
2025-04-12 06:28:34 +0000

    testing

---
+++ basicsjs/02datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3 ) // we are using nodejs, not browser
+
+console.log(3+
+    3)  // code readiablity should be high 
+
+let name = "ajay";
+let age = 18;
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null  => standalone value , its a object
+// undedefine => value did not assign
+// symbol => use for unique
+
+
+
+// object 
+
+console.log( typeof age);
+console.log( typeof null); //object
+console.log( typeof undefined); // undefined
---
+++ basicsjs/variable01.js
@@ -0,0 +1,21 @@
+const accountId = 13424;
+let accountEmail = "ajaynarwariya@gmail.com";
+var accountPassword = "123344";
+accountCity = "jaipur";
+
+let accountstate 
+// accountId = 24; not allowed 
+
+accountPassword = "1212121"
+accountEmail = "aj@google.com"
+
+
+/*
+prefer not to use var 
+
+try to to use let or const 
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity ,accountstate]);
