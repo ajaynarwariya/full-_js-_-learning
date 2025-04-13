d1ee66b77d678ca495134e6cd78538031314a293
Ajay Narwariya <100603751+ajaynarwariya@users.noreply.github.com>
2025-04-13 09:39:28 +0000

    string

---
+++ basicsjs/03string.js
@@ -0,0 +1,33 @@
+const name = "ajay"
+const repoCount = 10
+
+// console.log(name + repoCount + "value");
+
+console.log(` Hello my name is ${name} and repo count is ${repoCount}`);
+
+const love = new String("Ajay")
+console.log(love)
+
+//  console.log(love.__proto__);
+
+//  console.log(love.charAt(2));
+
+//  console.log(love.indexOf("y"));
+
+//  console.log(love.anchor());
+console.log(love.at(2));
+
+console.log(love.big());
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
